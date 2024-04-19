865f5ac5e06ad253548c4a222c99e9f65c39fca2
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-04-19 14:23:20 +0300

    Optimizing clipboard.lua

--- lua/neovim/config/clipboard.lua
+++ lua/neovim/config/clipboard.lua
@@ -1,6 +1,3 @@
--- This is to enable the WSL clipboard (found in the manual)
--- Additional for root: add /mnt/c/Windows/System32 to $PATH environment of root
-
 -- Let neovim use the default clipboard of the os
 vim.opt.clipboard:append("unnamedplus")
 
@@ -12,26 +9,35 @@ local function execute_command(command)
   return result
 end
 
--- Get the output of `uname -r`
+-- Get the output of `uname -r` to determine the current OS
 local uname_output = execute_command("uname -r")
 
 -- Determine if running on WSL based on uname output
 local is_wsl = uname_output:lower():find("microsoft") ~= nil
 
 -- Execute clipboard setup only if running on WSL
+-- Otherwise it will work without this on Linux
 if is_wsl then
+  -- This is to enable the WSL clipboard (found in the manual)
+  -- Additional for root: add /mnt/c/Windows/System32 to $PATH environment of root
+  local copyCmd = "clip.exe"
+  local powershell = "powershell.exe -c [Console]::Out.Write($("
+  local getClipboard = "Get-Clipboard -Raw"
+  local toStringReplace = ').tostring().replace("`r", ""))'
+  local pasteCmd = powershell .. getClipboard .. toStringReplace
+
   vim.cmd([[
-        let g:clipboard = {
-            \   'name': 'WslClipboard',
-            \   'copy': {
-                \      '+': 'clip.exe',
-                \      '*': 'clip.exe',
-            \    },
-            \   'paste': {
-                \      '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-                \      '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-            \   },
-            \   'cache_enabled': 0,
-        \ }
-    ]])
+    let g:clipboard = {
+      \ 'name': 'WslClipboard',
+      \ 'copy': {
+      \   '+': ']] .. copyCmd .. [[',
+      \   '*': ']] .. copyCmd .. [[',
+      \ },
+      \ 'paste': {
+      \   '+': ']] .. pasteCmd .. [[',
+      \   '*': ']] .. pasteCmd .. [[',
+      \ },
+      \ 'cache_enabled': 0,
+    \ }
+  ]])
 end
