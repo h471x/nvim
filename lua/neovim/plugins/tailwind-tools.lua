e2b90670aadc485b222c0c6479913631b39927ba
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-08-13 15:10:15 +0300

    Added TailWind-Tools Plugin

---
+++ lua/neovim/plugins/tailwind-tools.lua
@@ -0,0 +1,11 @@
+-- tailwind-tools.lua
+return {
+  "luckasRanarison/tailwind-tools.nvim",
+  name = "tailwind-tools",
+  build = ":UpdateRemotePlugins",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-telescope/telescope.nvim", -- optional
+  },
+  opts = {}, -- your configuration
+}
