fde18e2a336490c309a5bbd038ef455a004c5e6f
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-12-11 16:42:41 +0300

    feat[clipboard]: Check for WSL2 GUI

--- lua/neovim/config/clipboard.lua
+++ lua/neovim/config/clipboard.lua
@@ -21,29 +21,39 @@ local uname_output = execute_command("uname -r")
 -- Determine if running on WSL based on uname output
 local is_wsl = uname_output and uname_output:lower():find("microsoft") ~= nil
 
+-- Get the value of $DISPLAY to check if running under an RDP session (e.g., :13.0)
+local display_output = os.getenv("DISPLAY")
+
+-- Determine if RDP is active (based on $DISPLAY being :13.0)
+local rdp_active = display_output == ":13.0"
+
 -- Execute clipboard setup only if running on WSL
 -- Otherwise it will work without this on Linux
 if is_wsl then
-  -- This is to enable the WSL clipboard (found in the manual)
-  -- Additional for root: add /mnt/c/Windows/System32 to $PATH environment of root
-  local copyCmd = "clip.exe"
-  local powershell = "powershell.exe -c [Console]::Out.Write($("
-  local getClipboard = "Get-Clipboard -Raw"
-  local toStringReplace = ').tostring().replace("`r", ""))'
-  local pasteCmd = powershell .. getClipboard .. toStringReplace
+  -- only use clip.exe on WSL in case we are not
+  -- currently using it as RDP connection or WSL GUI
+  if not rdp_active then
+    -- This is to enable the WSL clipboard (found in the manual)
+    -- Additional for root: add /mnt/c/Windows/System32 to $PATH environment of root
+    local copyCmd = "clip.exe"
+    local powershell = "powershell.exe -c [Console]::Out.Write($("
+    local getClipboard = "Get-Clipboard -Raw"
+    local toStringReplace = ').tostring().replace("`r", ""))'
+    local pasteCmd = powershell .. getClipboard .. toStringReplace
 
-  vim.cmd([[
-    let g:clipboard = {
-      \ 'name': 'WslClipboard',
-      \ 'copy': {
-      \   '+': ']] .. copyCmd .. [[',
-      \   '*': ']] .. copyCmd .. [[',
-      \ },
-      \ 'paste': {
-      \   '+': ']] .. pasteCmd .. [[',
-      \   '*': ']] .. pasteCmd .. [[',
-      \ },
-      \ 'cache_enabled': 0,
-    \ }
-  ]])
+    vim.cmd([[
+      let g:clipboard = {
+        \ 'name': 'WslClipboard',
+        \ 'copy': {
+        \   '+': ']] .. copyCmd .. [[',
+        \   '*': ']] .. copyCmd .. [[',
+        \ },
+        \ 'paste': {
+        \   '+': ']] .. pasteCmd .. [[',
+        \   '*': ']] .. pasteCmd .. [[',
+        \ },
+        \ 'cache_enabled': 0,
+      \ }
+    ]])
+  end
 end
